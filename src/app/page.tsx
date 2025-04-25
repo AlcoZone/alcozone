82f38bbe4067a77a6eeb277b46c885105d13783e
Ricardo Garza <riccardogarza55@gmail.com>
2025-04-25 14:22:30 -0600

    Clean page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { MapWidget } from "@/components/MapWidget/MapWidget";
-
 export default function Home() {
   
   return (
     <div>
-      <MapWidget variant="predict" data={{}} />
     </div>
   );
 }
