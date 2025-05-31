36b9d9388c543a0bd25a3d13d41fc4f655a3981d
David Balleza Ayala <davidballezaa@gmail.com>
2025-05-30 18:54:49 -0600

    layout merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,27 @@
+"use client";
+
 import { AuthGuard } from "@/components/AuthGuard/AuthGuard";
 import "./globals.css";
 import { AuthProvider } from "@/providers/AuthProvider";
+import { protectedRoutes } from "@/utils/protectedRoutes";
+import { Menu } from "@/components/Menu/Menu";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [menuHidden, setMenuHidden] = useState(false);
+  const pathname = usePathname();
+  const isPublicRoute = protectedRoutes.public.has(pathname);
 
   return (
     <html lang="en">
       <body>
         <AuthProvider>
-          <AuthGuard>{children}</AuthGuard>
+          <AuthGuard>
+            {isPublicRoute ? <>{children}</> : <Menu>{children}</Menu>}
+          </AuthGuard>
         </AuthProvider>
       </body>
     </html>
